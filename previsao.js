051e1c8a8fba99bfcadd9a56a2f8f739c0280308
devantonio27 <antoniodev2709@gmail.com>
2024-03-04 21:47:33 -0300

    Arrumando layout, adicionando funcionalidade de previsão do tempo

---
+++ previsao.js
@@ -0,0 +1,6 @@
+async function getAddressByPrevisao() {
+    const latitude = document.getElementById('latitude').value
+    const longitude = document.getElementById('longitude').value
+
+    console.log(latitude) 
+}
