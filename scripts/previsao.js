7258503b6782bc41cd2e85d66729ad1053a4d7fe
devantonio27 <antoniodev2709@gmail.com>
2024-03-08 14:17:40 -0300

    criando função do botão, recebendo dados da API previsão

--- scripts/previsao.js
+++ scripts/previsao.js
@@ -3,10 +3,10 @@ async function getPrevisao() {
     const longitude = document.getElementById('longitude').value;
 
     try{
-        const response = await fetch(`https://api.open-meteo.com/v1/forecast?${latitude}&${longitude}&hourly=temperature_2m`)
+        const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`)
         const data = await response.json();
         console.log(data);        
-    }catch() {
-
+    }catch(error) {
+        alert(error.message)
     }
 }
