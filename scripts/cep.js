ac231107be18258fe2c794255cdb6433d7f31cf5
devantonio27 <antoniodev2709@gmail.com>
2024-03-27 15:45:08 -0300

    adicionando dados recebidos da API à tabela

--- scripts/cep.js
+++ scripts/cep.js
@@ -4,9 +4,9 @@ async function getAddressByCep() {
     const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
     const data = await response.json();
     console.log(data);
-    document.getElementById('rua').value = data.logradouro;
-    document.getElementById('bairro').value = data.bairro;
-    document.getElementById('cidade').value = data.localidade;
+    document.getElementById('rua').innerHTML = data.logradouro;
+    document.getElementById('bairro').innerHTML = data.bairro;
+    document.getElementById('cidade').innerHTML = data.localidade;
   } catch (error) {
     alert(error.message);
   }
